c4c13b459c6854ec157d589a930e5ad594ea57ff
Abelalex <61301404+Abelalex@users.noreply.github.com>
2020-02-27 15:10:39 -0400

    Add files via upload

---
+++ Vehiculo.cs
@@ -0,0 +1,32 @@
+namespace Vehiculo
+{
+  
+    class Vehículo_
+    {
+        public string marca = null;
+        public string modelo = null;
+
+        static void SetVehiculo()
+        {
+            string Marca = "BMW";
+            string Modelo = "Serie_3_Gran_Turismo";
+
+        }
+
+        static void verVehiculo()
+        {
+            Console.WriteLine("El vehículo es: ");
+            Console.ReadLine();
+
+
+        }
+
+        static void MostrarVehiculo()
+        {
+
+
+        }
+
+    }
+
+}
---
+++ Vehiculo3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehiculo2
+{
+    class mostrarVehiculo
+    {
+        static void Main(string[] args)
+        {
+            Vehículo Carro = new Vehículo();
+            Carro.SetVehiculo("Gran_Turismo", "BMW");
+            Carro.verVehiculo();
+            Console.WriteLine();
+
+            Vehículo Carro1 = new Vehículo();
+            Carro1.SetVehiculo("porsche", "ferrari");
+            Carro1.verVehiculo();
+            Console.ReadKey();
+
+        }
+    }
+}
